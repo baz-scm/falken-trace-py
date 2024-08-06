28d7d05fe873093a81382b66d1e19ba91fd00828
gruebel <anton.gruebel@gmail.com>
2024-08-06 20:34:10 +0200

    add test and run in CI

--- falken_trace/falken.py
+++ falken_trace/falken.py
@@ -21,7 +21,7 @@ def wrap_dd_span(wrapped: Callable[P, Span], _instance: Tracer, args: P.args, kw
     for frame in inspect.stack()[1:]:  # first frame is itself
         if frame.filename.startswith(work_dir) and "site-packages" not in frame.filename:
             span.set_tag("code.filepath", removeprefix(frame.filename, work_dir))
-            span.set_tag("code.lineno", frame.lineno)
+            span.set_tag("code.lineno", str(frame.lineno))
             span.set_tag("code.parent_func", frame.function)
             break
 

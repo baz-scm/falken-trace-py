0c18444a2a2451eb69f6a539b11f2f9373870af8
gruebel <anton.gruebel@gmail.com>
2024-08-06 23:14:32 +0200

    remove Python 3.13 tests for now

--- tests/test_falken.py
+++ tests/test_falken.py
@@ -12,6 +12,6 @@ def test_wrap_dd_span() -> None:
     # then
     tags = span.get_tags()
 
-    assert tags.get("code.filepath").endswith("/test_falken.py")
+    assert tags.get("code.filepath").endswith("test_falken.py")
     assert tags.get("code.lineno") == "10"
     assert tags.get("code.parent_func") == "test_wrap_dd_span"

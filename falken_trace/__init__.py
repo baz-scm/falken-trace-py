0fddf7da7832bfa55626949a4cb09868c6471e43
Anton Grübel <anton.gruebel@gmail.com>
2024-10-14 12:25:27 +0200

    fix: only patch FastAPI when it is used (#27)

--- falken_trace/__init__.py
+++ falken_trace/__init__.py
@@ -7,5 +7,5 @@ from falken_trace.web.fastapi import wrap_fastapi_entrypoint_span
 if config.env_vars_config.falken_trace_enabled:
     if config.env_vars_config.dd_trace_enabled:
         wrapt.wrap_function_wrapper("ddtrace", "Tracer.start_span", wrap_dd_span)
-
-    wrapt.wrap_function_wrapper("fastapi.routing", "run_endpoint_function", wrap_fastapi_entrypoint_span)
+    if config.env_vars_config.fastapi_installed:
+        wrapt.wrap_function_wrapper("fastapi.routing", "run_endpoint_function", wrap_fastapi_entrypoint_span)

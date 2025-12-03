1f282e8e661545ef3425fb4143726291007e9b2a
gruebel <anton.gruebel@gmail.com>
2025-12-03 21:58:52 +0100

    fix wrapt imports

--- falken_trace/__init__.py
+++ falken_trace/__init__.py
@@ -1,4 +1,4 @@
-import wrapt
+from wrapt.patches import wrap_function_wrapper
 
 from falken_trace.common import config
 from falken_trace.tracing.datadog import wrap_dd_span
@@ -7,9 +7,9 @@ from falken_trace.web.fastapi import wrap_fastapi_entrypoint_span
 if config.env_vars_config.falken_trace_enabled:
     if config.env_vars_config.dd_trace_enabled:
         try:
-            wrapt.wrap_function_wrapper("ddtrace.trace", "Tracer.start_span", wrap_dd_span)
+            wrap_function_wrapper("ddtrace.trace", "Tracer.start_span", wrap_dd_span)  # type:ignore[no-untyped-call]
         except AttributeError:
             # fallback to v2 import
-            wrapt.wrap_function_wrapper("ddtrace", "Tracer.start_span", wrap_dd_span)
+            wrap_function_wrapper("ddtrace", "Tracer.start_span", wrap_dd_span)  # type:ignore[no-untyped-call]
     if config.env_vars_config.fastapi_installed:
-        wrapt.wrap_function_wrapper("fastapi.routing", "run_endpoint_function", wrap_fastapi_entrypoint_span)
+        wrap_function_wrapper("fastapi.routing", "run_endpoint_function", wrap_fastapi_entrypoint_span)  # type:ignore[no-untyped-call]

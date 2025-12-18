37e9bf4e6aeaf6d3848cde1afa6ef674a6e1ac27
Anton Grübel <anton.gruebel@gmail.com>
2025-12-18 22:03:49 +0100

    chore: replace mypy with ty (#71)

--- falken_trace/__init__.py
+++ falken_trace/__init__.py
@@ -7,9 +7,9 @@ from falken_trace.web.fastapi import wrap_fastapi_entrypoint_span
 if config.env_vars_config.falken_trace_enabled:
     if config.env_vars_config.dd_trace_enabled:
         try:
-            wrap_function_wrapper("ddtrace.trace", "Tracer.start_span", wrap_dd_span)  # type:ignore[no-untyped-call]
+            wrap_function_wrapper("ddtrace.trace", "Tracer.start_span", wrap_dd_span)
         except AttributeError:
             # fallback to v2 import
-            wrap_function_wrapper("ddtrace", "Tracer.start_span", wrap_dd_span)  # type:ignore[no-untyped-call]
+            wrap_function_wrapper("ddtrace", "Tracer.start_span", wrap_dd_span)
     if config.env_vars_config.fastapi_installed:
-        wrap_function_wrapper("fastapi.routing", "run_endpoint_function", wrap_fastapi_entrypoint_span)  # type:ignore[no-untyped-call]
+        wrap_function_wrapper("fastapi.routing", "run_endpoint_function", wrap_fastapi_entrypoint_span)
